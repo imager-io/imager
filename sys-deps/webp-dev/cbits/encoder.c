4dd407dc776516e78f476145353b5753d288b4a7
colbyn <hello@colbyn.com>
2019-11-03 23:02:39 -0700

    update cbits

--- sys-deps/webp-dev/cbits/encoder.c
+++ sys-deps/webp-dev/cbits/encoder.c
@@ -2,21 +2,22 @@
 #include <webp/decode.h>
 #include <webp/types.h>
 #include <stdio.h>
+#include <assert.h>
 
 void encoder_stub() {
     printf("encoder_stub\n");
 }
 
 void webp_config_init(WebPConfig* config) {
-    WebPConfigInit(config);
+    assert(WebPConfigInit(config));
 }
 
 void webp_config_preset(WebPConfig* config, WebPPreset preset, float quality) {
-    WebPConfigPreset(config, preset, quality);
+    assert(WebPConfigPreset(config, preset, quality));
 }
 
 void webp_validate_config(WebPConfig* config) {
-    WebPValidateConfig(config);
+    assert(WebPValidateConfig(config));
 }
 
 
