5c81f46928ee01dafd292d85e688fd003a1c1855
colbyn <hello@colbyn.com>
2019-11-12 14:48:11 -0700

    Ports/NodeJS updates.
    Rename folder from imager-nodejs to nodejs.
    Misc stuff to get napi lib building.

--- ports/imager-nodejs/scripts/setup-mac.sh
+++
@@ -1,8 +0,0 @@
-# LINUX - CROSS COMPILE
-brew install -y FiloSottile/musl-cross/musl-cross
-brew tap SergioBenitez/osxct
-brew install -y filosottile/musl-cross/musl-cross
-brew install -y gcc
-
-# WINDOWS - CROSS COMPILE
-brew install -y mingw-w64
---
+++ ports/nodejs/scripts/setup-mac.sh
@@ -0,0 +1,8 @@
+# LINUX - CROSS COMPILE
+brew install FiloSottile/musl-cross/musl-cross
+brew tap SergioBenitez/osxct
+brew install filosottile/musl-cross/musl-cross
+brew install gcc
+
+# WINDOWS - CROSS COMPILE
+brew install mingw-w64
