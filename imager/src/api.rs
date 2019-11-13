b800dd53d70be21690f257c57f0ea12ba0c17576
colbyn <hello@colbyn.com>
2019-11-13 13:18:19 -0700

    better api

--- imager/src/api.rs
+++ imager/src/api.rs
@@ -2,14 +2,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 use crate::opt;
-use crate::data::OutputSize;
-pub use crate::data::Resolution;
+pub use crate::data::{Resolution, OutputSize};
 
 
-pub fn opt(input_image: &Vec<u8>, resize: Option<Resolution>) -> Result<Vec<u8>, String> {
-    let resize = resize
-        .map(|x| OutputSize::Px(x))
-        .unwrap_or(OutputSize::default());
+pub fn opt(input_image: &Vec<u8>, resize: OutputSize) -> Result<Vec<u8>, String> {
     let source = opt::Source::new(input_image, resize)?;
     let (output, opt_meta) = source.run_search();
     Ok(output)
