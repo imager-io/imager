8206e0da9efc54cf91e82c31672ef60f6d929065
colbyn <hello@colbyn.com>
2019-11-17 18:04:37 -0700

    cleanup

--- ports/nodejs/lib/index.test.ts
+++ ports/nodejs/lib/index.test.ts
@@ -13,6 +13,7 @@ test('ImageBuffer: #2 Buffer IO', async () => {
     const input_path = "assets/test/1.jpeg";
     const output_path = "assets/output/test/1-2.jpeg";
     if (!fs.existsSync("assets/output/test")){
+        fs.mkdirSync("assets/output");
         fs.mkdirSync("assets/output/test");
     }
     let data: Buffer = fs.readFileSync(input_path);
@@ -22,6 +23,5 @@ test('ImageBuffer: #2 Buffer IO', async () => {
         .then(x => x.to_buffer())
         .then(x => {
             fs.writeFileSync(output_path, x);
-            console.log("done");
         });
 });
