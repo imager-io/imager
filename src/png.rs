91524fed87ec03532d268215294e563f15a8698c
colbyn <hello@colbyn.com>
2019-11-29 16:25:05 -0700

    png dev - got basic encoder working

--- src/png.rs
+++ src/png.rs
@@ -1,7 +1,9 @@
+use std::convert::{AsRef, From};
 use std::io::{Read, Write, BufWriter};
 use std::process::exit;
 use std::iter::FromIterator;
 use std::iter::Extend;
+use std::path::{Path, PathBuf};
 use exoquant::optimizer::Optimizer;
 use exoquant::*;
 use lodepng::Bitmap;
@@ -9,20 +11,46 @@ use lodepng::RGBA;
 use image::{DynamicImage, GenericImage, GenericImageView};
 
 
-fn load_img(path: &str) -> Result<Bitmap<RGBA<u8>>, String> {
-    match lodepng::decode32_file(path) {
-        Ok(img) => Ok(img),
-        Err(_) => Err(format!("Error: Failed to load PNG '{}'.", path)),
+pub fn encode_indexed(palette: &[Color], image: &[u8], width: u32, height: u32) -> Vec<u8> {
+    let mut state = lodepng::State::new();
+    for color in palette {
+        unsafe {
+            lodepng::ffi::lodepng_palette_add(
+                &mut state.info_png_mut().color,
+                color.r,
+                color.g,
+                color.b,
+                color.a,
+            );
+            lodepng::ffi::lodepng_palette_add(
+                &mut state.info_raw_mut(),
+                color.r,
+                color.g,
+                color.b,
+                color.a,
+            );
+        }
     }
+    state.info_png_mut().color.set_bitdepth(8);
+    state.info_png_mut().color.colortype = lodepng::ColorType::PALETTE;
+    state.info_raw_mut().set_bitdepth(8);
+    state.info_raw_mut().colortype = lodepng::ColorType::PALETTE;
+    state.encode(image, width as usize, height as usize).expect("encode png data")
 }
 
-pub fn compress(num_colors: usize) {
+
+pub fn compress() {
+    // INPUT ARGUMENTS
+    let num_colors: usize = 256;
     assert!(num_colors <= 256);
-    let input_path = "assets/samples/noisy.png";
-    let output_path = "assets/output/main.png";
-    let img = ::image::open(input_path).expect("load input png");
-    let ditherer: Box<dyn ditherer::Ditherer> = Box::new(ditherer::None);
     let opt_level = 3;
+    // DEV
+    let input_path = "assets/samples/code.png";
+    let output_path = "assets/output/main.png";
+    // LOAD & DECODE
+    let img = ::image::open(input_path).expect("load input png");
+    // SETUP
+    let ditherer: Box<dyn ditherer::Ditherer> = Box::new(ditherer::None);
     let optimizer: Box<dyn Optimizer> = Box::new(optimizer::WeightedKMeans);
     let input_pixels = img
         .pixels()
@@ -46,127 +74,57 @@ pub fn compress(num_colors: usize) {
             quantizer = quantizer.optimize(&*optimizer, 4);
         }
     }
+    // PALETTE DATA
     let palette = quantizer.colors(&colorspace);
     let palette = optimizer.optimize_palette(&colorspace, &palette, &histogram, 8);
-    // let mut state = lodepng::State::new();
-    // for color in &palette {
-    //     unsafe {
-    //         lodepng::ffi::lodepng_palette_add(
-    //             &mut state.info_png().color,
-    //             color.r,
-    //             color.g,
-    //             color.b,
-    //             color.a,
-    //         );
-    //         lodepng::ffi::lodepng_palette_add(
-    //             &mut state.info_raw(),
-    //             color.r,
-    //             color.g,
-    //             color.b,
-    //             color.a,
-    //         );
-    //     }
-    // }
-    // state.info_png().color.bitdepth = 8;
-    // state.info_png().color.colortype = lodepng::ColorType::LCT_PALETTE;
-    // state.info_raw().bitdepth = 8;
-    // state.info_raw().colortype = lodepng::ColorType::LCT_PALETTE;
     let remapper = Remapper::new(&palette, &colorspace, &*ditherer);
+    // PIXEL DATA
     let out_data: Vec<u8> = remapper
         .remap_iter(Box::new(input_pixels.into_iter()), img.width() as usize)
         .collect();
-    // match state.encode_file(output_path, &out_data, img.width, img.height) {
-    //     Ok(_) => (),
-    //     Err(_) => {
-    //         panic!("Error: Failed to write PNG '{}'.", output_path);
-    //     }
-    // };
+    // ENCODE
+    let out_file = encode_indexed(
+        &palette,
+        &out_data,
+        img.width(),
+        img.height(),
+    );
+    // DONE
+    std::fs::write("test.png", &out_file);
 }
 
-pub fn compress_ref(num_colors: usize) {
-    let input_path = "assets/samples/noisy.png";
-    let output_path = "assets/output/main.png";
-    let img = load_img(input_path).expect("load input png");
-    let ditherer: Box<dyn ditherer::Ditherer> = Box::new(ditherer::None);
-    let (optimizer, opt_level): (Box<dyn Optimizer>, u32) = (Box::new(optimizer::WeightedKMeans), 3);
-    let histogram = img
-        .buffer
-        .as_ref()
-        .iter()
-        .map(|c| Color::new(c.r, c.g, c.b, c.a))
-        .collect();
-    let colorspace = SimpleColorSpace::default();
-    let mut quantizer = Quantizer::new(&histogram, &colorspace);
-    let kmeans_step = {
-        if opt_level < 2 {
-            num_colors
-        } else if opt_level == 2 {
-            (num_colors as f64).sqrt().round() as usize
-        } else {
-            1
-        }
-    };
-    while quantizer.num_colors() < num_colors {
-        quantizer.step();
-        if quantizer.num_colors() % kmeans_step == 0 {
-            quantizer = quantizer.optimize(&*optimizer, 4);
-        }
-    }
-    let palette = quantizer.colors(&colorspace);
-    let palette = optimizer.optimize_palette(&colorspace, &palette, &histogram, 8);
-    let mut state = lodepng::State::new();
-    for color in &palette {
-        unsafe {
-            lodepng::ffi::lodepng_palette_add(
-                &mut state.info_png().color,
-                color.r,
-                color.g,
-                color.b,
-                color.a,
-            );
-            lodepng::ffi::lodepng_palette_add(
-                &mut state.info_raw(),
-                color.r,
-                color.g,
-                color.b,
-                color.a,
-            );
-        }
-    }
-    state.info_png().color.bitdepth = 8;
-    state.info_png().color.colortype = lodepng::ColorType::LCT_PALETTE;
-    state.info_raw().bitdepth = 8;
-    state.info_raw().colortype = lodepng::ColorType::LCT_PALETTE;
-    let remapper = Remapper::new(&palette, &colorspace, &*ditherer);
-    let out_data: Vec<_> = remapper
-        .remap_iter(
-            Box::new(
-                img.buffer
-                    .as_ref()
-                    .iter()
-                    .map(|c| Color::new(c.r, c.g, c.b, c.a)),
-            ),
-            img.width,
-        )
-        .collect();
-    match state.encode_file(output_path, &out_data, img.width, img.height) {
-        Ok(_) => (),
-        Err(_) => {
-            panic!("Error: Failed to write PNG '{}'.", output_path);
-        }
-    };
-}
+// pub fn encode(data: Vec<u8>, width: u32, height: u32) {
+//     // SETUP
+//     // let mut buffer = BufWriter::new(Vec::<u8>::new());
+//     let path = Path::new("image.png");
+//     let file = std::fs::File::create(path).unwrap();
+//     let ref mut buffer = BufWriter::new(file);
+//     // RUN
+//     {
+//         let mut encoder = ::png::Encoder::new(buffer, width, height);
+//         encoder.set_depth(::png::BitDepth::Eight);
+//         // encoder.set_compression(png::Compression::Best);
+//         // encoder.set_filter(png::FilterType::Paeth);
+//         encoder.set_color(::png::ColorType::Indexed);
+//         // ENCODE
+//         let mut writer = encoder
+//             .write_header()
+//             .expect("write png header");
+//         writer
+//             .write_image_data(&data)
+//             .expect("write/encode png data");
+//     }
+//     // DONE
+//     // buffer
+//     //     .into_inner()
+//     //     .expect("extract encoded png buffer")
+// }
 
-pub fn encode(width: u32, height: u32, data: Vec<u8>) -> Vec<u8> {
-    let writer = Vec::<u8>::new();
-    let mut encoder = ::png::Encoder::new(writer, width, height);
-    encoder.set_depth(::png::BitDepth::Eight);
-    encoder.set_compression(png::Compression::Best);
-    encoder.set_filter(png::FilterType::Paeth);
-    encoder.set_color(::png::ColorType::RGB);
-    unimplemented!()
-}
+
+///////////////////////////////////////////////////////////////////////////////
+// DEV
+///////////////////////////////////////////////////////////////////////////////
 
 pub fn run() {
-    compress(20);
+    compress();
 }
