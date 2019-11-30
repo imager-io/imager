f6225c9a6dd0cd731b587bbd68a247e02bb852ed
colbyn <hello@colbyn.com>
2019-11-29 19:26:17 -0700

    png dev - basic support for text based png graphics

--- src/png.rs
+++ src/png.rs
@@ -10,8 +10,21 @@ use lodepng::Bitmap;
 use lodepng::RGBA;
 use image::{DynamicImage, GenericImage, GenericImageView};
 
+///////////////////////////////////////////////////////////////////////////////
+// DATA TYPES
+///////////////////////////////////////////////////////////////////////////////
 
-pub fn encode_indexed(palette: &[Color], image: &[u8], width: u32, height: u32) -> Vec<u8> {
+#[derive(Debug, Clone, PartialEq)]
+pub enum ImageMode {
+    Text,
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////
+
+fn encode_indexed(palette: &[Color], image: &[u8], width: u32, height: u32) -> Vec<u8> {
     let mut state = lodepng::State::new();
     for color in palette {
         unsafe {
@@ -38,93 +51,75 @@ pub fn encode_indexed(palette: &[Color], image: &[u8], width: u32, height: u32) 
     state.encode(image, width as usize, height as usize).expect("encode png data")
 }
 
-
-pub fn compress() {
-    // INPUT ARGUMENTS
-    let num_colors: usize = 256;
+pub fn compress(source: &DynamicImage, mode: ImageMode, num_colors: usize) -> Result<Vec<u8>, String> {
+    // CHECKS
     assert!(num_colors <= 256);
-    let opt_level = 3;
-    // DEV
-    let input_path = "assets/samples/code.png";
-    let output_path = "assets/output/main.png";
-    // LOAD & DECODE
-    let img = ::image::open(input_path).expect("load input png");
     // SETUP
-    let ditherer: Box<dyn ditherer::Ditherer> = Box::new(ditherer::None);
-    let optimizer: Box<dyn Optimizer> = Box::new(optimizer::WeightedKMeans);
-    let input_pixels = img
+    let (ditherer, optimizer) = match mode {
+        ImageMode::Text => {
+            // VALUES
+            let d = ditherer::None;
+            let o = optimizer::WeightedKMeans;
+            // DONE
+            let d: Box<dyn ditherer::Ditherer> = Box::new(d);
+            let o: Box<dyn Optimizer> = Box::new(o);
+            (d, o)
+        }
+    };
+    let input_pixels = source
         .pixels()
         .map(|(_, _, px)| Color::new(px.0[0], px.0[1], px.0[2], px.0[3]))
         .collect::<Vec<Color>>();
     let histogram = Histogram::from_iter(input_pixels.clone());
     let colorspace = SimpleColorSpace::default();
-    let mut quantizer = Quantizer::new(&histogram, &colorspace);
-    let kmeans_step = {
-        if opt_level < 2 {
-            num_colors
-        } else if opt_level == 2 {
-            (num_colors as f64).sqrt().round() as usize
-        } else {
-            1
-        }
+    let colorspace = SimpleColorSpace {
+        gamma: 0.85,
+        dither_gamma: 2.2,
+        transparency_scale: 0.01,
+        scale: Colorf {
+            r: 0.6,
+            g: 0.6,
+            b: 0.6,
+            a: 0.5,
+        },
     };
-    while quantizer.num_colors() < num_colors {
+    let mut quantizer = Quantizer::new(&histogram, &colorspace);
+    for _ in 0..num_colors {
         quantizer.step();
-        if quantizer.num_colors() % kmeans_step == 0 {
-            quantizer = quantizer.optimize(&*optimizer, 4);
-        }
+        quantizer = quantizer.optimize(&*optimizer, 16);
     }
     // PALETTE DATA
     let palette = quantizer.colors(&colorspace);
-    let palette = optimizer.optimize_palette(&colorspace, &palette, &histogram, 8);
+    let palette = optimizer.optimize_palette(&colorspace, &palette, &histogram, 16);
     let remapper = Remapper::new(&palette, &colorspace, &*ditherer);
     // PIXEL DATA
     let out_data: Vec<u8> = remapper
-        .remap_iter(Box::new(input_pixels.into_iter()), img.width() as usize)
+        .remap_iter(Box::new(input_pixels.into_iter()), source.width() as usize)
         .collect();
     // ENCODE
     let out_file = encode_indexed(
         &palette,
         &out_data,
-        img.width(),
-        img.height(),
+        source.width(),
+        source.height(),
     );
     // DONE
-    std::fs::write("test.png", &out_file);
+    Ok(out_file)
 }
-
-// pub fn encode(data: Vec<u8>, width: u32, height: u32) {
-//     // SETUP
-//     // let mut buffer = BufWriter::new(Vec::<u8>::new());
-//     let path = Path::new("image.png");
-//     let file = std::fs::File::create(path).unwrap();
-//     let ref mut buffer = BufWriter::new(file);
-//     // RUN
-//     {
-//         let mut encoder = ::png::Encoder::new(buffer, width, height);
-//         encoder.set_depth(::png::BitDepth::Eight);
-//         // encoder.set_compression(png::Compression::Best);
-//         // encoder.set_filter(png::FilterType::Paeth);
-//         encoder.set_color(::png::ColorType::Indexed);
-//         // ENCODE
-//         let mut writer = encoder
-//             .write_header()
-//             .expect("write png header");
-//         writer
-//             .write_image_data(&data)
-//             .expect("write/encode png data");
-//     }
-//     // DONE
-//     // buffer
-//     //     .into_inner()
-//     //     .expect("extract encoded png buffer")
-// }
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // DEV
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn run() {
-    compress();
+    // DEV
+    let input_path = "assets/samples/code.png";
+    let output_path = "assets/output/main.png";
+    // LOAD & DECODE
+    let img = ::image::open(input_path).expect("load input png");
+    // RUN
+    let mode = ImageMode::Text;
+    let num_colors = 6;
+    let out = compress(&img, mode, num_colors).expect("compress png source");
+    std::fs::write("assets/output/test.png", &out);
 }
